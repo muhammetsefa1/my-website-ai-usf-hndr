12fb8d4437aab2d2ac38355be8dcf52e3e840c47
muhammetsefa1 <sefahundur1@gmail.com>
2024-11-22 00:25:50 -0500

    Yeni dosyalar eklendi ve düzenlendi

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import About from './About';
 import Home from './Home';
+import './App.css';
 
 const App = () => {
   return (
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './home.css'; // CSS dosyasını içe aktarıyoruz
+import myImage from './assets/images/AI.jpg'; // Resminizi doğru yoldan içe aktarın
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
+    <div className="image-container">
+      <img src={myImage} alt="Blended Effect" />
     </div>
   );
 };
