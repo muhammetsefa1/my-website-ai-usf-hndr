ccf8dd6026ce0c315aa65362539ddb7f827274e1
Ynshndr <ynshndr@gmail.com>
2024-11-30 20:53:40 +0300

    my branch first commit to changes
    sayfa başlığı üniversite adı olarak değiştirildi, ve dosyalardaki boş boşluklar silindi. welcome yazısı ortalandı. AI resmide içerik yazısıyla ortalandı.

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css'; // Header stil dosyasını import ediyoruz
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css"; // Header stil dosyasını import ediyoruz
 
 const Header = () => {
   return (
     <header>
-      <h1 className="intro-head">Welcome to My Website</h1>
+      <h1 className="intro-head">University Of South Florida</h1>
       <nav>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import './Home.css'; // CSS dosyasını içe aktarıyoruz
-import myImage from './assets/images/AI.jpg'; // Resminizi doğru yoldan içe aktarın
+import React from "react";
+import "./Home.css"; // CSS dosyasını içe aktarıyoruz
+import myImage from "./assets/images/AI.jpg"; // Resminizi doğru yoldan içe aktarın
 
 const Home = () => {
   return (
     <div>
-
       <div className="intro-container">
         <h1 className="intro-title">Welcome to My Website</h1>
         <p className="intro-text">
-          Discover amazing content and features tailored just for you. Explore our services and learn more about what we offer.
+          Discover amazing content and features tailored just for you. Explore
+          our services and learn more about what we offer.
         </p>
       </div>
       <div className="image-container">
         <img src={myImage} alt="Blended Effect" />
       </div>
-
-
     </div>
   );
 };
