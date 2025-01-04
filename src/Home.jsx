8d7cc1a6d6b85c7c15fbb973f6b3ffadd9f06f73
muhammetsefa2 <162960315+muhammetsefa1@users.noreply.github.com>
2025-01-04 20:40:47 +0300

    sayfa boyut düzenlemesi

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,7 @@ import instagramLogo from './assets/images/instagram-logo.png';
 import twitterLogo from './assets/images/twitter-logo.png';
 import emailLogo from './assets/images/email-logo.png';
 import myImage from './assets/images/AI.jpg';
+import Background from './assets/images/Background.jpg';
 import profile1 from './assets/images/profile1.jpg'; //profil fotoğraflarını import edin
 import profile2 from './assets/images/profile2.jpg'
 import profile3 from './assets/images/profile3.jpg'
@@ -11,34 +12,22 @@ const Home = () => {
   return (
     <div>
       <div className="background" id="about-section">
-        <div className="intro-container">
-          <h1 className="intro-title"></h1>
-        </div>
-        <div className="image-container">
-          <div class="text-content">
-            <h2>Başlik</h2>
-            <p>Bu, fotoğrafın sol tarafında duran bir metin örneğidir.</p>
-          </div>
-          <img src={myImage} alt="AI Visual" class="photo"/>
+        <img src={Background} alt="Back Visual" class="background"/>
+      </div>
+      <div className="image-container">
+        <img src={myImage} alt="AI Visual" className="photo" />
+        <div className="text-content">
+          <h2>Başlik</h2>
+          <p>Bu, fotoğrafın sol tarafında duran bir metin örneğidir.asgagadgsdfhdbznrg</p>
         </div>
       </div>
+
 
       <div className="gray-section2" id="events-section">
         <h2>AI Club Innovations</h2>
       </div>
       
-      <div class="gray-section"></div>
-      <div className="social-container">
-        <a href="https://www.instagram.com/taic.usf/" target="_blank" rel="noopener noreferrer">
-          <img src={instagramLogo} alt="Instagram Logo" className="social-logo" />
-        </a>
-        <a href="https://twitter.com/yourclubusername" target="_blank" rel="noopener noreferrer">
-          <img src={twitterLogo} alt="Twitter Logo" className="social-logo" />
-        </a>
-        <a href="mailto:sefahundur1@gmail.com" target="_blank" rel="noopener noreferrer">
-          <img src={emailLogo} alt="Email" className="social-logo" />
-        </a>
-      </div>
+      
 
       {/* Profil fotoğraflarının altına isim ve çalışma alanı eklemek */}
       <div className="profile-container" id="profile-section">
@@ -142,23 +131,35 @@ const Home = () => {
         </a>
         
       </div>
-      <div className="form-link-container">
-        <h2>Join Our Club</h2>
-        <p>Fill out our membership form by clicking the button below:</p>
-        <a
-          href="https://docs.google.com/forms/d/e/1FAIpQLScsJCBN1UTnn8phNAKUq5BENVj76xVgUOk7zM0bPpSgz4HjHA/viewform?usp=dialog" // Google Form linkini buraya ekleyin
-          target="_blank"
-          rel="noopener noreferrer"
-          className="form-button"
-        >
-          Fill Out the Form
-        </a>
-      </div>
+        <div className="form-link-container">
+          <h2>Join Our Club</h2>
+          <p>Fill out our membership form by clicking the button below:</p>
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLScsJCBN1UTnn8phNAKUq5BENVj76xVgUOk7zM0bPpSgz4HjHA/viewform?usp=dialog" // Google Form linkini buraya ekleyin
+            target="_blank"
+            rel="noopener noreferrer"
+            className="form-button"
+          >
+            Fill Out the Form
+          </a>
+        </div>
 
       <div>
       
-    </div>
-
+      </div>
+      <div class="gray-section">
+        <div className="social-container">
+          <a href="https://www.instagram.com/taic.usf/" target="_blank" rel="noopener noreferrer">
+            <img src={instagramLogo} alt="Instagram Logo" className="social-logo" />
+          </a>
+          <a href="https://twitter.com/yourclubusername" target="_blank" rel="noopener noreferrer">
+            <img src={twitterLogo} alt="Twitter Logo" className="social-logo" />
+          </a>
+          <a href="mailto:sefahundur1@gmail.com" target="_blank" rel="noopener noreferrer">
+            <img src={emailLogo} alt="Email" className="social-logo" />
+          </a>
+        </div>
+      </div>
 
     </div>
   );
